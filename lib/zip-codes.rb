240fb1da42c093abdf75c30858f19ad9a9c6927f
Rick Peyton <peytorb@gmail.com>
2016-01-27 21:22:46 -0600

    Trying a second method and an if statement. Not the prettiest code, but here goes

--- lib/zip-codes.rb
+++ lib/zip-codes.rb
@@ -5,7 +5,11 @@ module ZipCodes
 
   class << self
     def identify(code)
-      db[code]
+      if ('A'..'Z').include?(code[0].upcase)
+        ca_db[code[0..2].upcase]
+      else
+        db[code]
+      end
     end
 
     def db
@@ -13,6 +17,14 @@ module ZipCodes
         this_file = File.expand_path(File.dirname(__FILE__))
         us_data = File.join(this_file, 'data', 'US.yml')
         YAML.load(File.open(us_data))
+      end
+    end
+
+    def db
+      @db ||= begin
+        this_file = File.expand_path(File.dirname(__FILE__))
+        ca_data = File.join(this_file, 'data', 'CA.yml')
+        YAML.load(File.open(ca_data))
       end
     end
 
